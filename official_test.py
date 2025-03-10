d72104b4ee580bfbec2acf24d42e9bccc3ce7a8d
“shen.yiru” <shen.yiru@synyi.com>
2025-03-10 13:25:57 +0800

    delete device part

--- official_test.py
+++ official_test.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")
 
-model(data=r"/home/shenyiru/pythonProject/ultralytics/ultralytics/assets/bus.jpg", save=True, device=0)
+model(data=r"/home/shenyiru/pythonProject/ultralytics/ultralytics/assets/bus.jpg", save=True)
